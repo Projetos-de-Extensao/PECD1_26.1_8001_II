40e16314757bc38aab47526d5bce8fa8592bb2e0
TheCaruzo <gabriel_caruzo@hotmail.com>
2026-05-25 11:06:04 -0300

    Feat: Componente do Footer

---
+++ my-react-app/src/jsx/Footer.jsx
@@ -0,0 +1,12 @@
+import '../css/index.css';
+
+
+function Footer() {
+  return (
+<footer className="rodape">
+        <p>&copy; 2026 IBMEC. Todos os direitos reservados.</p>
+      </footer>
+  );
+}
+
+export default Footer;
