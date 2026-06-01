1d36b36f97498a07af188856e7274ef1e79dfe42
TheCaruzo <gabriel_caruzo@hotmail.com>
2026-06-01 08:08:30 -0300

    Fix: Resolvido o erro do PErfil onde não estava rederizando o componente do perfil.

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,29 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route, useLocation, Navigate } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes, useLocation } from 'react-router-dom'
+
 import './css/index.css'
-import Login from './pages/LoginPage.jsx'
-import Perfil from './pages/PerfilPage.jsx'
-import Solicitacoes from './pages/SolicitacoesPage.jsx'
-import Home from './pages/HomePage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import PerfilPage from './pages/PerfilPage.jsx'
+import SolicitacoesPage from './pages/SolicitacoesPage.jsx'
+import HomePage from './pages/HomePage.jsx'
 import Navbar from './jsx/Navbar.jsx'
 import Footer from './jsx/Footer.jsx'
 
-
 function AppLayout() {
   const location = useLocation()
-  const hideNavOn = ['/', '/login'] 
-  const hideFooterOn = ['/login', '/'] 
+
+  const hideNavOn = ['/', '/login']
+  const hideFooterOn = ['/', '/login']
+
   return (
     <>
       {!hideNavOn.includes(location.pathname) && <Navbar />}
+
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/perfil" element={<Perfil />} />
-        <Route path="/solicitacoes" element={<Solicitacoes />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/perfil" element={<PerfilPage />} />
+        <Route path="/solicitacoes" element={<SolicitacoesPage />} />
       </Routes>
+
       {!hideFooterOn.includes(location.pathname) && <Footer />}
     </>
   )
---
+++ my-react-app/src/pages/PerfilPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Perfil from "../jsx/Perfil.jsx";
+import '../css/perfil.css'
+import '../css/index.css';
+
+
+
+export default function PerfilPage() {
+    return (
+    <>
+    <Perfil />
+    </>
+    )
+}
